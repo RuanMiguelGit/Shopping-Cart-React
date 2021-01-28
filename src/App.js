cf14f7533e86fb7f31198dfc80a87df4160f4dae
Ruan Miguel <ruanmiguelps@gmail.com>
2021-01-28 20:21:16 -0300

    change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Search from './pages/Search';
+import ProductListing from './components/ProductsListing';
 
 function App() {
   return (
