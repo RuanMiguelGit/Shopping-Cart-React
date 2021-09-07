d53042306406cc242d03f53339f71acd3561f022
Ruan Miguel <ruanmiguelps@gmail.com>
2021-09-07 09:43:01 -0300

    Updates

---
+++ precache-manifest.3919d8d64d339701da0d8c66395f1d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e08513d3d64508456cbe196303a71eb",
+    "url": "/Shopping-Cart-React/index.html"
+  },
+  {
+    "revision": "b3360d28f3967d07fd9b",
+    "url": "/Shopping-Cart-React/static/css/main.587dcd36.chunk.css"
+  },
+  {
+    "revision": "ec3c64eae3526aab3269",
+    "url": "/Shopping-Cart-React/static/js/2.d374ad53.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Shopping-Cart-React/static/js/2.d374ad53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3360d28f3967d07fd9b",
+    "url": "/Shopping-Cart-React/static/js/main.2f060c22.chunk.js"
+  },
+  {
+    "revision": "a5744b247567ab02be41",
+    "url": "/Shopping-Cart-React/static/js/runtime-main.865b4b64.js"
+  },
+  {
+    "revision": "c35c7dea7f39f704dd16864e27b97b59",
+    "url": "/Shopping-Cart-React/static/media/shoppingCartIcon.c35c7dea.png"
+  }
+]);
